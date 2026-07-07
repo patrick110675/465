09db88af47432e7f884ce241d6717277a7e7c99e
patrick110675 <f22598426@gmail.com>
2026-07-07 11:20:23 +0800

    Add files via upload

---
+++ firebase-config.example.js
@@ -0,0 +1,10 @@
+// 之後建立 Firebase 專案後，把這個檔案複製成 firebase-config.js
+// 再貼上 Firebase Console 提供的設定。
+window.firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT.appspot.com",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID"
+};
---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// Firebase 正式上線時，把 Firebase 專案設定貼在這裡。
+// 目前先保持空白，系統會使用瀏覽器本機資料模式。
+window.firebaseConfig = {
+  apiKey: '',
+  authDomain: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: '',
+  appId: ''
+};
